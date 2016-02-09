1d485554bdc3be4d6a4b1d2d1516bbdedea6c409
Sascha Bienert <sascha.bienert@fyber.com>
2016-02-09 12:58:10 +0100

    adds a project for native AdMob SDK integration

---
+++ AdMob/AdMobNativeTestApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HeyzapNativeTestApp
+//
+//  Created by Sascha Bienert on 03/02/16.
+//  Copyright © 2016 Fyber GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIView *bannerContainerView;
+
+@end
+
