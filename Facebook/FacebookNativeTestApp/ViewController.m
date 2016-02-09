1c41590bc25bd245d92f29e5ba680dcc7527e317
Sascha Bienert <sascha.bienert@fyber.com>
2016-02-09 14:16:18 +0100

    adds callback for facebook delegate

--- Facebook/FacebookNativeTestApp/ViewController.m
+++ Facebook/FacebookNativeTestApp/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 @import FBAudienceNetwork;
 
-@interface ViewController ()
+@interface ViewController () <FBAdViewDelegate>
 
 @property (weak, nonatomic) FBAdView *banner;
 
@@ -19,7 +19,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     [self.bannerContainerView setHidden:YES];
 }
 
@@ -29,14 +28,16 @@
 }
 
 - (IBAction)loadBanner:(id)sender {
-        FBAdView *adView = [[FBAdView alloc] initWithPlacementID:@"660126697415740_973155002779573"
-                                                          adSize:kFBAdSizeHeight50Banner
-                                              rootViewController:self];
-        [adView loadAd];
+    FBAdView *adView = [[FBAdView alloc] initWithPlacementID:@"660126697415740_973155002779573"
+                                                      adSize:kFBAdSizeHeight50Banner
+                                          rootViewController:self];
+    [adView setDelegate:self];
+    [adView loadAd];
 
-        [self releaseBanner];
-        self.banner = adView;
-        [self.bannerContainerView addSubview:self.banner];
+    [self releaseBanner];
+    self.banner = adView;
+    [self.banner setFrame:CGRectMake(0, 0, self.bannerContainerView.frame.size.width, self.bannerContainerView.frame.size.height)];
+    [self.bannerContainerView addSubview:self.banner];
 }
 
 - (IBAction)showBanner:(id)sender {
@@ -54,6 +55,39 @@
 - (void)releaseBanner {
     [self.banner removeFromSuperview];
     self.banner = nil;
+}
+
+#pragma mark - FBAdViewDelegate
+
+- (void)adView:(FBAdView *)adView didFailWithError:(NSError *)error;
+{
+    NSLog(@"[FBAdViewDelegate] Ad failed to load");
+}
+
+- (void)adViewDidLoad:(FBAdView *)adView;
+{
+    NSLog(@"[FBAdViewDelegate] Ad was loaded and ready to be displayed");
+}
+
+- (void)adViewDidClick:(FBAdView *)adView
+{
+    NSLog(@"[FBAdViewDelegate] The user clicked on the ad and will be taken to its destination.");
+}
+
+- (void)adViewDidFinishHandlingClick:(FBAdView *)adView
+{
+    NSLog(@"[FBAdViewDelegate] The user finished to interact with the ad.");
+}
+
+- (void)adViewWillLogImpression:(nonnull FBAdView *)adView
+{
+    NSLog(@"[FBAdViewDelegate] The ad view will log an impression.");
+}
+
+- (UIViewController *)viewControllerForPresentingModalView
+{
+    NSLog(@"[FBAdViewDelegate] The Facebook banner ad asks for viewControllerForPresentingModalView.");
+    return self;
 }
 
 @end
