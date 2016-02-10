08599ed7d361f7fae16e2271cb1bb05b02f592bb
Sascha Bienert <sascha.bienert@fyber.com>
2016-02-10 16:51:24 +0100

    adds delegate log messages

--- AdMob/AdMobNativeTestApp/ViewController.m
+++ AdMob/AdMobNativeTestApp/ViewController.m
@@ -151,14 +151,14 @@
 /// the banner view to the view hierarchy if it hasn't been added yet.
 - (void)adViewDidReceiveAd:(GADBannerView *)bannerView
 {
-
+    NSLog(@"[GADBannerViewDelegate] ad request successfully received an ad");
 }
 
 /// Tells the delegate that an ad request failed. The failure is normally due to network
 /// connectivity or ad availablility (i.e., no fill).
 - (void)adView:(GADBannerView *)bannerView didFailToReceiveAdWithError:(GADRequestError *)error
 {
-
+    NSLog(@"[GADBannerViewDelegate] ad request failed");
 }
 
 #pragma mark Click-Time Lifecycle Notifications
@@ -167,20 +167,20 @@
 /// an ad. The delegate may want to pause animations and time sensitive interactions.
 - (void)adViewWillPresentScreen:(GADBannerView *)bannerView
 {
-
+    NSLog(@"[GADBannerViewDelegate] full screen view will be presented in response to the user clicking on an ad");
 }
 
 /// Tells the delegate that the full screen view will be dismissed.
 - (void)adViewWillDismissScreen:(GADBannerView *)bannerView
 {
-
+    NSLog(@"[GADBannerViewDelegate] the full screen view will be dismissed");
 }
 
 /// Tells the delegate that the full screen view has been dismissed. The delegate should restart
 /// anything paused while handling adViewWillPresentScreen:.
 - (void)adViewDidDismissScreen:(GADBannerView *)bannerView
 {
-
+    NSLog(@"[GADBannerViewDelegate] the full screen view has been dismissed");
 }
 
 /// Tells the delegate that the user click will open another app, backgrounding the current
@@ -188,7 +188,7 @@
 /// are called immediately before this method is called.
 - (void)adViewWillLeaveApplication:(GADBannerView *)bannerView
 {
-    
+    NSLog(@"[GADBannerViewDelegate] the user click will open another app");
 }
 
 @end
